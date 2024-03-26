3a0637a5f0e214c4f97c5969ad08f4c3728813c3
Fernando Jose Lopez Lopez <flopelop@emeal.nttdata.com>
2024-03-26 07:15:35 +0100

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  //https://github.com/Klerith/angular-bases/tree/fin-seccion-5
+  public title: string = 'Mi primera app de Angular';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/hero.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroModule,
+    DbzModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
